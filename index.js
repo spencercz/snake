bcf3d755a32941849287e7fc791e97ea100f8e3e
spencercz <48261199+spencercz@users.noreply.github.com>
2020-10-11 20:26:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   let name = "user";
   $("#setname").click(()=>{
+    name = document.getElementById("name").val();
     document.getElementById("yourname").innerText = `<h2>Your name: ${name}</h2>`;
   }
 });
